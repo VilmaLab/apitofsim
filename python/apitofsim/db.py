2ab0e74fdc28de4896678b5d93606ecf4ef80d84
Frankie Robertson <frankie@robertson.name>
2026-01-22 08:56:52 +0200

    Add a pathway_report view

--- python/apitofsim/db.py
+++ python/apitofsim/db.py
@@ -333,6 +333,40 @@ create table experiment_failure (
 
 
 REPORT_VIEW = """
+create or replace view pathway_report as
+select
+    -- Pathway info
+    p.id as pathway_id,
+
+    -- Cluster info (the main cluster)
+    c.id as cluster_id,
+    c.common_name as cluster_common_name,
+    c.atomic_mass as cluster_atomic_mass,
+    c.electronic_energy as cluster_electronic_energy,
+    c.rotational_temperatures as cluster_rotational_temperatures,
+    c.vibrational_temperatures as cluster_vibrational_temperatures,
+
+    -- Product 1 info
+    p1.id as product1_id,
+    p1.common_name as product1_common_name,
+    p1.atomic_mass as product1_atomic_mass,
+    p1.electronic_energy as product1_electronic_energy,
+    p1.rotational_temperatures as product1_rotational_temperatures,
+    p1.vibrational_temperatures as product1_vibrational_temperatures,
+
+    -- Product 2 info
+    p2.id as product2_id,
+    p2.common_name as product2_common_name,
+    p2.atomic_mass as product2_atomic_mass,
+    p2.electronic_energy as product2_electronic_energy,
+    p2.rotational_temperatures as product2_rotational_temperatures,
+    p2.vibrational_temperatures as product2_vibrational_temperatures,
+
+from pathway p
+inner join cluster c on c.id = p.cluster_id
+inner join cluster p1 on p1.id = p.product1_id
+inner join cluster p2 on p2.id = p.product2_id;
+
 create or replace view experiment_report as
 select
     -- Experiment run info
