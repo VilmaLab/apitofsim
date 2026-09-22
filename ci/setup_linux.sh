1372dfa8849d7d66c0013a941c1eb8ca6245c6b6
Frankie Robertson <frankier@frankier.name>
2026-09-22 11:46:45 +0300

    Make setup_linux run in strict mode

--- ci/setup_linux.sh
+++ ci/setup_linux.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
 dnf -y install clang clang-libs curl
-bash ci/install_tbb.sh
+./ci/install_tbb.sh
 PIPX_DEFAULT_PYTHON=/usr/bin/python3 pipx install -f patchelf==0.19.1.0rc1
